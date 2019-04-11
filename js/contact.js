c944a6021d8b90d2c2dd61faad56861946cdf047
kchang00 <k_chang@fanshaweonline.ca>
2019-04-11 13:17:17 -0400

    project buttons/imgs, merch hero/js arrays, nav js/css, topBtn js

--- js/contact.js
+++ js/contact.js
@@ -9,8 +9,8 @@
 			btnDownHero			= document.querySelectorAll('.downArrow'),
 			closeContactRemind	= document.querySelector('#close'),
 			contactRemind 		= document.querySelector('#announcement'),
-	 	 	scrollTop = 0,
-			counter   = 0;
+	 	 	scrollTop 			= 0,
+			counter   			= 0;
 			
 
   	//functions
@@ -23,18 +23,20 @@
 	window.onscroll = function(){
 		var pageScroll = window.pageYOffset;  
 
-		if (pageScroll > scrollTop && mediaQuery1.matches){
+		if (pageScroll > 200) {
+			if (pageScroll > scrollTop && mediaQuery1.matches){
 			mainHeader.style.top = '-100%';
 			mainHeaderCon.style.top = '-100%';
+			}
+			else {
+				mainHeaderCon.style.top = '0';
+			}
+		   scrollTop = pageScroll;
 		}
-		else if (pageScroll === 0 && mediaQuery1.matches) {
+		else if (pageScroll < 50 && mediaQuery1.matches) {
 			mainHeader.style.top = '0';
 			mainHeaderCon.style.top = '52px';
 		}
-		else {
-			mainHeaderCon.style.top = '0';
-		}
-	   scrollTop = pageScroll;
 	}
 
 	function skipHero(e) {
